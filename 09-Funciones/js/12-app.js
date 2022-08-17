cce60bed4e4c1eceb9acabf72a0a3d3c72d2438b
NicolasAValderrama <nicovalderrama2002@gmail.com>
2022-08-17 16:46:42 -0300

    Modulo funciones finalizado

---
+++ 09-Funciones/js/12-app.js
@@ -0,0 +1,22 @@
+//metodo map
+const carrito = [
+    { nombre: 'Monitor 27 pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+];
+
+carrito.forEach( p => {
+    console.log(`Nombre: ${p.nombre} - Precio: ${p.precio}`);
+});
+
+//la diferencia entre map y forEach es que el map retorna un nuevo array con los valores modificados
+const nuevoCarrito = carrito.map( p => {
+    return {
+        nombre: p.nombre,
+        precio: p.precio * 0.8
+    }
+});
+
